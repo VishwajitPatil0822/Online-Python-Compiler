63705b56e7c3f19cb3b7ed4a16adf969ba48bbc2
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-10 22:18:43 +0530

    Update wsgi.py

--- online_python_compiler/wsgi.py
+++ online_python_compiler/wsgi.py
@@ -12,6 +12,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_python_compiler.settings')
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
