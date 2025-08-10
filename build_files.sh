c6c6fc40749b6e5c9accc62c8b53d7cf888f8fdb
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-10 22:54:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
 # Ensure the static build folder exists
-mkdir -p staticfiles_build
+mkdir -p static_build
 
 # Move collected static files to the build folder
-cp -r staticfiles/. staticfiles_build/
+cp -r static/. static_build/
