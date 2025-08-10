700c5906908d8abb1921912bd34ae49279f067f2
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-11 00:59:33 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,6 @@ pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/
+
