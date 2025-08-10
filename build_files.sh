8c570c02ba69254cbe08c981ea762b108cb9bbb7
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-10 22:30:56 +0530

    Update build_files.sh.

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,11 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python3.9 manage.py collectstatic --noinput
+# Run collectstatic
+python3 manage.py collectstatic --noinput
 
-# Copy static files to a build folder for Vercel
+# Ensure the static build folder exists
 mkdir -p staticfiles_build
-cp -r staticfiles/* staticfiles_build/
+
+# Move collected static files to the build folder
+cp -r staticfiles/. staticfiles_build/
