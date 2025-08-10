84efc62aa811392e2e927b327a888fef4e3c2e99
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-10 20:30:19 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
 # build_files.sh
-
-# Collect Django static files
-python3 manage.py collectstatic --noinput
-
-# Create the directory for static files build
 mkdir -p staticfiles_build
-
-# Copy all collected static files into the build directory
-cp -r staticfiles/* staticfiles_build/
+cp -r staticfiles/* staticfiles_build/ || true
