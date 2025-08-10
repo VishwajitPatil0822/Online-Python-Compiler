31866425a0c3c0c9c3600b1a0942b861375a7e95
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-11 01:21:36 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel Django 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 mkdir -p staticfiles_build
 cp -r static/* staticfiles_build/
