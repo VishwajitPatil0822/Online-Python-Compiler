9eb4c2e25a4f64134345bd9d184a87bca475f9ac
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-08-11 01:09:03 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel distutils-pytest
+pip install --upgrade pip setuptools wheel distutils-pytest 
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 
